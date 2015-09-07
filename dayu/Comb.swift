67e4aacc77dc427929c7f7d4d9aff1b1e1f9cf56
killpigman <280532379@qq.com>
2015-09-07 16:18:19 +0800

    更行

--- dayu/Comb.swift
+++ dayu/Comb.swift
@@ -24,8 +24,12 @@ class Comb: NSObject {
     var yesFollow = 0
     var yseSupport = 0
     
+    var contentLabelWidth:CGFloat = 280
+    var MAXFLOAT = 26;
+    
     func parse(dict:NSDictionary) {
-        id = dict["id"] as String
+        var t: AnyObject? = dict["id"]
+        id = "\(t)"
         createTime = (dict["create_time"] as NSString).longLongValue
         grade = dict["grade"] as Int
         drawdown = dict["drawdown"] as Float
@@ -41,4 +45,42 @@ class Comb: NSObject {
         yseSupport = dict["yessupport"] as Int
     }
     
+//    func getNameWidth() -> CGFloat {
+//        var size = name.textSizeWithFont(UIFont.systemFontOfSize(FONT_SIZE), constrainedToSize: CGSizeMake(20000, 22))
+////        CGSize titleSize = [aString sizeWithFont,font constrainedToSize:CGSizeMake(MAXFLOAT, 30)]
+////        if imageNum > 0 {
+////            imageGroupHeight = defaultImageGroupHeight
+////        }
+////        var text: NSString = NSString(CString: name.cStringUsingEncoding(NSUTF8StringEncoding)!,
+////            encoding: NSUTF8StringEncoding)
+////        text.sizeWithAttributes(attrs: [NSObject : AnyObject]!)
+//        
+//        return contentLabelOffsetY + size.height + imageGroupHeight + marginTop * 3 + 30
+//    }
+    
+    func getColor(s: Int, g: Int) -> [String] {
+        var colors = [String](count: 4, repeatedValue: "")
+        if s < 20 {
+            colors[0] = "comb_circle_1"
+            colors[1] = g > 0 ? "comb_up_1" : "comb_down_1"
+            colors[2] = "#999999"
+        } else if s < 40 {
+            colors[0] = "comb_circle_2"
+            colors[1] = g > 0 ? "comb_up_2" : "comb_down_2"
+            colors[2] = "#397ada"
+        } else if s < 60 {
+            colors[0] = "comb_circle_3"
+            colors[1] = g > 0 ? "comb_up_3" : "comb_down_3"
+            colors[2] = "#44b74f"
+        } else if s < 80 {
+            colors[0] = "comb_circle_4"
+            colors[1] = g > 0 ? "comb_up_4" : "comb_down_4"
+            colors[2] = "#fda126"
+        } else {
+            colors[0] = "comb_circle_5"
+            colors[1] = g > 0 ? "comb_up_5" : "comb_down_5"
+            colors[2] = "#ef5a3a"
+        }
+        return colors
+    }
 }
--- dayu/CombListViewController.swift
+++ dayu/CombListViewController.swift
@@ -7,16 +7,42 @@
 //
 
 import UIKit
-class CombListViewController: BaseUIViewController ,UITableViewDataSource, UITableViewDelegate {
+class CombListViewController: BaseUIViewController ,UITableViewDataSource, UITableViewDelegate, UIActionSheetDelegate{
     
 
+    @IBOutlet weak var UbTimeType: UIButton!
+    @IBOutlet weak var UbType: UIButton!
     @IBOutlet weak var UtvCombs: UITableView!
     private var combList = NSMutableArray();
+    private var type : String = "pro"
+    private var timeType : String = "S"
+    var refreshControl = UIRefreshControl()
+    
+    var typeStr = ["pro", "drawdown", "grade","hot"]
+    var timeTypeStr = ["D", "W", "M","S"]
+    var select = [type, timeType]
+    var ubsTypeStr = ["排序 收益", "排序 波动", "排序 得分","排序 人气"]
+    var ubsTimeTypeStr = ["日", "周", "月","总"]
+    var typeFlag = [0,0]
+    
+    
+    @IBAction func type(sender: AnyObject) {
+        selectType()
+    }
+    
+    
+    @IBAction func timeType(sender: AnyObject) {
+        selectTimeType()
+    }
+    
     
     func getCombList() {
-        var params = ["token":app.getToken(), "type":"pro", "timetype":"S", "startnum":0]
+        println(type + "--" + timeType)
+        combList.removeAllObjects()
+        var params = ["token":app.getToken(), "type":type, "timetype":timeType, "startnum":0]
         HttpUtil.post(URLConstants.getSortCombinationsUrl, params: params, success: {(data:AnyObject!) in
-            println("combs data = \(data)")
+            self.refreshControl.endRefreshing()
+//            println("combs data = \(data)")
             if data["stat"] as String == "OK" {
                 self.combList.removeAllObjects()
                 var array = data["combinations"] as NSArray
@@ -30,11 +56,16 @@ class CombListViewController: BaseUIViewController ,UITableViewDataSource, UITab
             }, failure:{(error:NSError!) in
                 //TODO 处理异常
                 println(error.localizedDescription)
+                self.refreshControl.endRefreshing()
         })
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        refreshControl.addTarget(self, action: "getCombList", forControlEvents: UIControlEvents.ValueChanged)
+        refreshControl.attributedTitle = NSAttributedString(string: "加载更多")
+        refreshControl.frame.size = CGSizeMake(320, 20)
+        UtvCombs.addSubview(refreshControl)
         getCombList();
         // Do any additional setup after loading the view.
     }
@@ -43,7 +74,42 @@ class CombListViewController: BaseUIViewController ,UITableViewDataSource, UITab
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func selectType() {
+        var asType = UIActionSheet(title: "请选择", delegate: self, cancelButtonTitle: "取消", destructiveButtonTitle: nil, otherButtonTitles: "收益", "波动", "得分","人气")
+        asType.actionSheetStyle = UIActionSheetStyle.BlackTranslucent
+        asType.tag = 0
+        asType.showInView(self.view)
+    }
+    func selectTimeType() {
+        var asTimeType = UIActionSheet(title: "请选择", delegate: self, cancelButtonTitle: "取消", destructiveButtonTitle: nil, otherButtonTitles: "日", "周", "月","总")
+        asTimeType.actionSheetStyle = UIActionSheetStyle.BlackTranslucent
+        asTimeType.tag = 1
+        asTimeType.showInView(self.view)
+    }
+    
+    func actionSheet(actionSheet: UIActionSheet, clickedButtonAtIndex buttonIndex: Int) {
+        var ubsStrs = [ubsTypeStr,ubsTimeTypeStr]
+        var selectStr = [typeStr,timeTypeStr]
+        var ubs = [UbType,UbTimeType]
+        var tag = actionSheet.tag
+        println("tag == \(tag)")
+        if buttonIndex == 0 {
+            return
+        } else {
+            println("selectStr[tag][buttonIndex - 1] == " + selectStr[tag][buttonIndex - 1])
+            //select[tag] = selectStr[tag][buttonIndex - 1]
+            if tag == 0 {
+                type = selectStr[tag][buttonIndex - 1]
+            } else {
+                timeType = selectStr[tag][buttonIndex - 1]
+            }
+            println("ubsStrs[tag][buttonIndex - 1] == " + ubsStrs[tag][buttonIndex - 1])
+            ubs[tag].titleLabel?.text = ubsStrs[tag][buttonIndex - 1]
+            typeFlag[tag] = buttonIndex - 1
+            getCombList()
+        }
+    }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var comb = combList[indexPath.row] as Comb
@@ -64,14 +130,19 @@ class CombListViewController: BaseUIViewController ,UITableViewDataSource, UITab
         var ulGrade = cell.viewWithTag(21) as UILabel
         
 //        ivPhoto.sd_setImageWithURL(NSURL(string: URLConstants.getImageUrl(group)), placeholderImage: UIImage(named: "user_default_photo.png"))
-        
+        var color = comb.getColor(comb.grade, g: comb.gradego)
+        uivCircle.image = UIImage(named:color[0]);
+        uivArrow.image = UIImage(named: color[1])
         ulTime.text = StringUtil.formatTime(comb.createTime) + " \(comb.userName)创建"
         ulName.text = comb.name
         ulSupportNum.text = String(comb.supportNum)
         ulFollowtNum.text = String(comb.followNum)
-        ulBodonglv.text = String(comb.gradego)
+        ulBodonglv.text = String(StringUtil.formatFloat(comb.drawdown) + "%")
+        ulBodonglv.textColor = StringUtil.colorWithHexString(color[2])
         ulGrade.text = String(comb.grade)
-        
+        ulDataInCircle.text = StringUtil.formatFloat(comb.pro) + "%"
+        ulTypeInCircle.text = "\(ubsTimeTypeStr[typeFlag[1]])收益"
+//        ulTime.lineBreakMode = UILineBreakModeCharacterWrap
         return cell
     }
     
